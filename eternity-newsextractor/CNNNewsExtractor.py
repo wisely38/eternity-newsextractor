94badd01fd76454925701ca55db0ea13dd7b501f
Ko Wai yan <wisely38@gmail.com>
2018-06-12 21:03:54 -0700

    dev - added function to extract word frequencies and figure

--- eternity-newsextractor/CNNNewsExtractor.py
+++ eternity-newsextractor/CNNNewsExtractor.py
@@ -1,7 +1,12 @@
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 import nltk
 import newspaper
 import time
+from nltk.tokenize import sent_tokenize, word_tokenize, regexp_tokenize
+from nltk.corpus import stopwords
+import operator
 from dao.PostgresDao import PostgresDao
 
 
@@ -19,6 +24,58 @@ class CNNNewsExtractor:
         self.cnn_paper = newspaper.build(self.cnn_url)
         self.article_df = pd.DataFrame(columns=self.columns)
         nltk.download('punkt')
+        nltk.download('stopwords')
+
+    def get_filename(self, article_df, index):
+        filename = article_df.loc[index, 'article_id'] + '.txt'
+        return filename
+
+    def get_wordfrequencies_dist(self, text):
+        wordsFiltered = self.filterStopwords(text)
+        freq = nltk.FreqDist(wordsFiltered)
+        return freq
+
+    def get_wordfrequencies_df(self, text):
+        freq = self.get_wordfrequencies_dist(text)
+        sorted_freq = sorted(
+            freq.items(), key=operator.itemgetter(1), reverse=True)
+        df = pd.DataFrame(sorted_freq, columns=['word', 'count'])
+        selected_df = df.loc[df['count'] > 1, :]
+        return selected_df
+
+    def plotHistograms(self, freq, filename):
+        fig = plt.figure()
+        # selected_df.plot(x=selected_df['word'], kind='bar', figsize=(30, 10))
+        freq.plot(50, cumulative=False)
+        fig.savefig(filename.replace('.txt', '.png'), dpi=fig.dpi)
+        # plt.show()
+
+    def filterStopwords(self, text):
+        stop_words = set(stopwords.words('english'))
+        stop_words.update(['desired', 'years', 'year', 'francisco', 'apply', 'writing', 'e.g', '/', '’', '-', '.',
+                           ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])  # remove it if you need punctuation
+        pattern = r'[a-zA-Z]\w+'
+        nltk_tokens = regexp_tokenize(text, pattern)
+        wordsFiltered = []
+        for w in nltk_tokens:
+            if w.lower() not in stop_words:
+                wordsFiltered.append(w.lower())
+        return wordsFiltered
+
+    def process_article_insights(self):
+        postgres_df = self.dao.read_table(self.table_name)
+        for i in range(0, len(postgres_df.loc[:, 'article_id'])):
+            filename = self.get_filename(postgres_df, i)
+            with open(filename, 'r') as reader:
+                text = reader.read()
+            selected_df, freq = self.get_wordfrequencies_df(text)
+            self.plotHistograms(freq, filename)
+            selected_df.to_csv(filename.replace(
+                '.txt', '.csv'), sep=',', encoding='utf-8')
+
+    # def retrieveTextBody(self):
+
+    #     return text
 
     def extract_site_metadata(self):
         for article in self.cnn_paper.articles:
@@ -64,11 +121,12 @@ class CNNNewsExtractor:
             self.write_to_db(combined_postgres_df)
 
     def write_to_file(self, article_df):
-        for i in range(1, len(article_df.loc[:, 'article_id'])):
-            filename = article_df.loc[i, 'article_id'] + '.txt'
-        with open(filename, 'w') as output:
-            print('writing out ', filename)
-            output.writelines(article_df.loc[i, 'body'])
+        for i in range(0, len(article_df.loc[:, 'article_id'])):
+            # filename = article_df.loc[i, 'article_id'] + '.txt'
+            filename = self.get_filename(article_df, i)
+            with open(filename, 'w') as output:
+                print('Writing out file: ', filename)
+                output.writelines(article_df.loc[i, 'body'])
 
     def write_to_db(self, article_df):
         article_postgres_df = article_df.drop('body', 1)
