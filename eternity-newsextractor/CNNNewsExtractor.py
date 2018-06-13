6807734f8cfcbd7ec8d03686253cbb8ea8723cd2
Ko Wai yan <wisely38@gmail.com>
2018-06-12 22:21:28 -0700

    dev - fixed code styles + add command runner classes

--- eternity-newsextractor/CNNNewsExtractor.py
+++ eternity-newsextractor/CNNNewsExtractor.py
@@ -16,6 +16,8 @@ class CNNNewsExtractor:
         self.table_name = 'cnn_news'
         self.columns = ['article_id', 'category',
                         'url', 'authors', 'keywords', 'body']
+        self.table_parameters = ['cnn_news', 'article_id',
+                                 'category', 'url', 'authors', 'keywords']
         self.cnn_paper = newspaper.build(self.cnn_url)
         self.article_df = pd.DataFrame(columns=self.columns)
         nltk.download('punkt')
@@ -25,6 +27,7 @@ class CNNNewsExtractor:
         return filename
 
     def extract_site_metadata(self):
+        print("Starting method extract_site_metadata...")
         for article in self.cnn_paper.articles:
             article.download()
             time.sleep(2)
@@ -52,33 +55,41 @@ class CNNNewsExtractor:
 
         # check if record already exists
         if not self.dao.has_table(self.table_name):
-            self.dao.create_table(
-                ['cnn_news', 'article_id', 'category', 'url', 'authors', 'keywords'])
+            print("Creates new table...")
+            self.dao.create_table(self.table_parameters)
+            print("Writes to new table...")
             self.write_to_db(article_df)
         elif self.dao.has_records(
-                self.table_name, self.columns[0], article_df.loc[:, ['article_id']].values.tolist()):
+                self.table_name, self.columns[0],
+                article_df.loc[:, ['article_id']].values.tolist()):
+            print("Read table from DB...")
             existing_postgres_df = self.dao.read_table('cnn_news')
             combined_postgres_df = pd.concat(
                 [self.article_df, existing_postgres_df])
             combined_postgres_df.drop_duplicates(
                 subset='article_id', keep='first', inplace=True)
+            print("Delete whole table from DB...")
             self.dao.delete_table('cnn_news')
-            self.dao.create_table(
-                ['cnn_news', 'article_id', 'category', 'url', 'authors', 'keywords'])
+            print("Recreate whole table to DB...")
+            self.dao.create_table(self.table_parameters)
             self.write_to_db(combined_postgres_df)
+        print("Done method extract_site_metadata.")
 
     def write_to_file(self, article_df):
+        print("Starting method write_to_file...")
         for i in range(0, len(article_df.loc[:, 'article_id'])):
             # filename = article_df.loc[i, 'article_id'] + '.txt'
             filename = self.get_filename(article_df, i)
             with open(filename, 'w') as output:
                 print('Writing out file: ', filename)
                 output.writelines(article_df.loc[i, 'body'])
+        print("Done method write_to_file...")
 
     def write_to_db(self, article_df):
+        print("Starting method write_to_db...")
         article_postgres_df = article_df.drop('body', 1)
-        self.dao.write_table(article_postgres_df,
-                             ['cnn_news', 'article_id', 'category', 'url', 'authors', 'keywords'])
+        self.dao.write_table(article_postgres_df, self.table_parameters)
+        print("Done method write_to_db...")
 
 
 def run(self):
---
+++ eternity-newsextractor/CNNNewsNlpAnalyzerRunner.py
@@ -0,0 +1,8 @@
+from CNNNewsNlpAnalyzer import CNNNewsNlpAnalyzer
+
+def main():
+    extractor = CNNNewsNlpAnalyzer()
+    extractor.extract_site_metadata()
+
+if __name__ == "__main__":
+    main()
